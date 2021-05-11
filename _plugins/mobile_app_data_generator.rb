b194150394f512ce905a50543068d40c5d418d07
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2021-05-11 14:18:33 +0200

    Added number_of_courses, number_of_videos and number_of_topics per category for mobile app json.

--- _plugins/mobile_app_data_generator.rb
+++ _plugins/mobile_app_data_generator.rb
@@ -1,12 +1,28 @@
 module MobileAppDataGeneratorPlugin
 
-  class Generator < Jekyll::Generator
-    def generate(site)
+  # Note:
+  #  number_of_videos in course is using English numerals and converted to Bangla in the mobile app
+  #  However, number_of_courses, number_of_videos and number_of_topics is in Bangla numerals.
+  #  Not need to convert on mobile app side.
 
+  class Generator < Jekyll::Generator
+    priority :lowest
+    def generate(site)
       categories = Common.read_categories(site)
       categories.each do | category |
         category["courses"] = Common.read_category_courses(site, category["id"])
         category["topics"] = Common.read_category_topics(site, category["id"])
+        category["number_of_courses"] = Common.to_bangla_number(category["courses"].size)
+
+        # Count total videos and unique topics in a category
+        total_videos = 0
+        total_topics = []
+        category["courses"].each do | course |
+          total_videos += course["number_of_videos"]
+          total_topics += course["topics"]
+        end
+        category["number_of_videos"] = Common.to_bangla_number(total_videos)
+        category["number_of_topics"] = Common.to_bangla_number(total_topics.uniq.size)
       end
 
       # Delete category if no courses
